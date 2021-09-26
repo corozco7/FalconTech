3f7da2e0ddd4d412a8b31726e6412037e69eb166
Laura Rios <laurarios414@gmail.com>
2021-09-26 10:35:28 -0500

    Archivo productos iniciales

---
+++ src/main/java/modelo/Productos_DTO.java
@@ -0,0 +1,10 @@
+package modelo;
+
+public class Productos_DTO {
+private int codigo;
+private int NIT_Proveedor;
+private String nombre;
+private double precio_compra;
+private double IVA_compra;
+private double precio_producto;
+}
